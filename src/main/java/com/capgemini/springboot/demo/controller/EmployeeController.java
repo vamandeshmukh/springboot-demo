ec6a1468573203fc4cdd8120ce84e2b364b0c2e9
vamandeshmukh <vamandeshmukh@gmail.com>
2022-03-24 12:54:31 +0530

    springbootdemo

--- src/main/java/com/capgemini/springboot/demo/controller/EmployeeController.java
+++ src/main/java/com/capgemini/springboot/demo/controller/EmployeeController.java
@@ -2,20 +2,26 @@ package com.capgemini.springboot.demo.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.capgemini.springboot.demo.model.Employee;
+import com.capgemini.springboot.demo.service.EmployeeService;
 
 @RestController
 public class EmployeeController {
 
+//	EmployeeService service = new EmployeeService();
+	@Autowired
+	private EmployeeService service;
+
 //	http://localhost:8088/get-employee-by-id
 	@GetMapping("/get-employee-by-id")
 	public Employee getEmployeeById() {
-		Employee emp = new Employee(101, "Sonu", 50000);
-		System.out.println(emp.toString());
-		return emp;
+		int employeeId = 101;
+		System.out.println("EmployeeController getEmployeeById");
+		return service.getEmpById(employeeId);
 	}
 
 //	http://localhost:8088/get-all-employees
@@ -38,3 +44,44 @@ public class EmployeeController {
 //}
 
 }
+
+//package com.capgemini.springboot.demo.controller;
+//
+//import java.util.List;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.capgemini.springboot.demo.model.Employee;
+//
+//@RestController
+//public class EmployeeController {
+//
+////	http://localhost:8088/get-employee-by-id
+//	@GetMapping("/get-employee-by-id")
+//	public Employee getEmployeeById() {
+//		Employee emp = new Employee(101, "Sonu", 50000);
+//		System.out.println(emp.toString());
+//		return emp;
+//	}
+//
+////	http://localhost:8088/get-all-employees
+//	@GetMapping("/get-all-employees")
+//	public List<Employee> getAllEmployees() {
+//		System.out.println("get-all-employees");
+//		return null;
+//	}
+//
+////  public Employee addEmployee() {
+////		return null;
+////	}
+//
+////	public Employee updateEmployee() {
+////	return null;
+////}
+//
+////	public Employee deleteEmployee() {
+////	return null;
+////}
+//
+//}
--- src/main/java/com/capgemini/springboot/demo/service/EmployeeService.java
+++ src/main/java/com/capgemini/springboot/demo/service/EmployeeService.java
@@ -2,7 +2,17 @@ package com.capgemini.springboot.demo.service;
 
 import org.springframework.stereotype.Service;
 
+import com.capgemini.springboot.demo.model.Employee;
+
 @Service
 public class EmployeeService {
+
+	public Employee getEmpById(int employeeId) {
+		System.out.println("EmployeeService getEmpById");
+//		Employee emp = get data from DB using repository
+		Employee emp = new Employee(employeeId, "Sonu", 50000);
+		System.out.println(emp.toString());
+		return emp;
+	}
 
 }

b32a280a9f503659eaf32aafc6d643903a0fda71
vamandeshmukh <vamandeshmukh@gmail.com>
2022-03-25 10:44:14 +0530

    responseentity
    cc

--- src/main/java/com/capgemini/springboot/demo/controller/EmployeeController.java
+++ src/main/java/com/capgemini/springboot/demo/controller/EmployeeController.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,14 +27,27 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService service;
 
+////	http://localhost:8088/get-employee-by-id/{eid}
+//	@GetMapping("/get-employee-by-id/{eid}")
+//	public ResponseEntity<Employee> getEmployeeById(@PathVariable(name = "eid") int employeeId) {
+//		LOG.info("EmployeeController getEmployeeById " + employeeId);
+//		Employee emp = service.getEmpById(employeeId);
+//		ResponseEntity<Employee> response = new ResponseEntity<>(emp, HttpStatus.OK);
+//		return response;
+//	}
+
 //	http://localhost:8088/get-employee-by-id/{eid}
 	@GetMapping("/get-employee-by-id/{eid}")
-	public Employee getEmployeeById(@PathVariable(name = "eid") int employeeId) {
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable(name = "eid") int employeeId) {
 		LOG.info("EmployeeController getEmployeeById " + employeeId);
-		return service.getEmpById(employeeId);
+		Employee emp = service.getEmpById(employeeId);
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("message", "Employee " + employeeId + " was found successfully.");
+		ResponseEntity<Employee> response = new ResponseEntity<>(emp, headers, HttpStatus.OK);
+		return response;
 	}
 
-//	http://localhost:8088/get-all-employees
+	// http://localhost:8088/get-all-employees
 	@GetMapping("/get-all-employees")
 	public List<Employee> getAllEmployees() {
 		LOG.info("get-all-employees");
@@ -60,6 +76,69 @@ public class EmployeeController {
 	}
 
 }
+
+//package com.capgemini.springboot.demo.controller;
+//
+//import java.util.List;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.DeleteMapping;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.PutMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.capgemini.springboot.demo.model.Employee;
+//import com.capgemini.springboot.demo.service.EmployeeService;
+//
+//@RestController
+//public class EmployeeController {
+//
+//	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
+//
+//	@Autowired
+//	private EmployeeService service;
+//
+////	http://localhost:8088/get-employee-by-id/{eid}
+//	@GetMapping("/get-employee-by-id/{eid}")
+//	public Employee getEmployeeById(@PathVariable(name = "eid") int employeeId) {
+//		LOG.info("EmployeeController getEmployeeById " + employeeId);
+//		return service.getEmpById(employeeId);
+//	}
+//
+////	http://localhost:8088/get-all-employees
+//	@GetMapping("/get-all-employees")
+//	public List<Employee> getAllEmployees() {
+//		LOG.info("get-all-employees");
+//		return service.getAllEmps();
+//	}
+//
+////	http://localhost:8088/add-employee 
+//	@PostMapping("/add-employee")
+//	public Employee addEmployee(@RequestBody Employee employee) {
+//		LOG.info(employee.toString());
+//		return service.addEmp(employee);
+//	}
+//
+////	http://localhost:8088/update-employee 
+//	@PutMapping("/update-employee")
+//	public Employee updateEmployee(@RequestBody Employee employee) {
+//		LOG.info(employee.toString());
+//		return service.updateEmp(employee);
+//	}
+//
+////	http://localhost:8088/delete-employee/{eid} 
+//	@DeleteMapping("/delete-employee/{eid}")
+//	public Employee deleteEmployee(@PathVariable(name = "eid") int employeeId) {
+//		LOG.info(Integer.toString(employeeId));
+//		return service.deleteEmp(employeeId);
+//	}
+//
+//}
 
 //package com.capgemini.springboot.demo.controller;
 //

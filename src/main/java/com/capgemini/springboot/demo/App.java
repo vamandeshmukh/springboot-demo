51cdd34279dfb18ba5f0dc92a07f5078b99cdf3d
vamandeshmukh <vamandeshmukh@gmail.com>
2022-03-25 17:31:16 +0530

    exception created

--- src/main/java/com/capgemini/springboot/demo/App.java
+++ src/main/java/com/capgemini/springboot/demo/App.java
@@ -5,14 +5,20 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringbootDemoApplication {
+/**
+ * 
+ * @author Vaman Deshmukh
+ *
+ */
 
-	private static final Logger LOG = LoggerFactory.getLogger(SpringbootDemoApplication.class);
+@SpringBootApplication
+public class App {
+
+	private static final Logger LOG = LoggerFactory.getLogger(App.class);
 
 	public static void main(String[] args) {
 		LOG.info("Start");
-		SpringApplication.run(SpringbootDemoApplication.class, args);
+		SpringApplication.run(App.class, args);
 		LOG.info("End");
 	}
 

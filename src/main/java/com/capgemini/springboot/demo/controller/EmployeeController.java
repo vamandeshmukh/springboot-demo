cbdaf2d4eff5f99942fb22d020db07bf2900c6f0
vamandeshmukh <vamandeshmukh@gmail.com>
2022-03-25 09:52:06 +0530

    requestbody

--- src/main/java/com/capgemini/springboot/demo/controller/EmployeeController.java
+++ src/main/java/com/capgemini/springboot/demo/controller/EmployeeController.java
@@ -6,8 +6,11 @@ import java.util.Random;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.capgemini.springboot.demo.model.Employee;
@@ -36,20 +39,22 @@ public class EmployeeController {
 		return service.getAllEmps();
 	}
 
+//	http://localhost:8088/add-employee 
 	@PostMapping("/add-employee")
-	public Employee addEmployee() {
-		Employee employee = new Employee();
-		LOG.info("Employee added successfully.");
+	public Employee addEmployee(@RequestBody Employee employee) {
+		LOG.info(employee.toString());
 		return service.addEmp(employee);
 	}
 
-//	public Employee updateEmployee() {
-//	return null;
-//}
+	@PutMapping("/update-employee")
+	public Employee updateEmployee() {
+		return null;
+	}
 
-//	public Employee deleteEmployee() {
-//	return null;
-//}
+	@DeleteMapping("/delete-employee")
+	public Employee deleteEmployee() {
+		return null;
+	}
 
 }
 

b7d731335dec285c03c5ba2f6ec380565c6d8c90
vamandeshmukh <vamandeshmukh@gmail.com>
2022-03-30 10:25:34 +0530

    JUnit Mockito

--- src/main/java/com/capgemini/springboot/demo/controller/EmployeeController.java
+++ src/main/java/com/capgemini/springboot/demo/controller/EmployeeController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -80,6 +81,8 @@ public class EmployeeController implements IEmployeeController {
 //	http://localhost:8088/emp/update-employee 
 	@Override
 	@PutMapping("/update-employee")
+//	@PostMapping("/update-employee")
+//	@PatchMapping("/update-employee")
 	public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee) {
 		LOG.info(employee.toString());
 		Employee emp = service.updateEmp(employee);
--- src/main/java/com/capgemini/springboot/demo/exception/CustomExceptionHandler.java
+++ src/main/java/com/capgemini/springboot/demo/exception/CustomExceptionHandler.java
@@ -7,9 +7,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
+// AOP 
+
+//@ControllerAdvice
+@RestControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
 	private final Logger LOG = LoggerFactory.getLogger(this.getClass());
